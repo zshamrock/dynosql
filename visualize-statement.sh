4e7a5402f872ac47eb2926fa64ce11044e609734
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2018-03-13 09:03:00 +0100

    Split statements into 2 lines

--- visualize-statement.sh
+++ visualize-statement.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-export CLASSPATH=${CLASSPATH}:target/classes && java org.antlr.v4.gui.TestRig com.akazlou.dynosql.antlr4.SQLite parse -gui
+export CLASSPATH=${CLASSPATH}:target/classes && \
+    java org.antlr.v4.gui.TestRig com.akazlou.dynosql.antlr4.SQLite parse -gui
